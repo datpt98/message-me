c8fab5d70a7019d6ce3e69d13818a7e3ec706aa5
Dat Pham Tien <datpt98@gmail.com>
2020-01-17 04:04:36 +0200

    final commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-//= require jquery
-//= require semantic-ui
 
+require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import "jquery"
+import "semantic-ui"
 
 
 $(document).on('turbolinks:load', function() {
 	$('.ui.dropdown').dropdown();
+	$('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
 
 
